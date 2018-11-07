cffa31e73e5a9632c5e3f1065198f8d259fb8771
Judzhin Miles <info@msbios.com>
2018-11-07 13:16:00 +0200

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -6,18 +6,10 @@
 
 namespace MSBios\Deploy;
 
-use MSBios\Deploy\Controller\IndexController;
 use MSBios\ModuleInterface;
-use Zend\Config\Config;
-use Zend\EventManager\EventInterface;
 use Zend\Loader\AutoloaderFactory;
 use Zend\Loader\StandardAutoloader;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\BootstrapListenerInterface;
-use Zend\Router\Http\Method;
-use Zend\Router\Http\Segment;
-use Zend\Router\Http\TreeRouteStack;
-use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Class Module
@@ -26,7 +18,7 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 class Module implements ModuleInterface, AutoloaderProviderInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.13';
+    const VERSION = '1.0.14';
 
     /**
      * @return mixed
@@ -34,47 +26,6 @@ class Module implements ModuleInterface, AutoloaderProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
-    }
-
-    /**
-     * Listen to the bootstrap event
-     *
-     * @param EventInterface $e
-     * @return array
-     */
-    public function onBootstrap(EventInterface $e)
-    {
-        ///** @var ServiceLocatorInterface $serviceLocator */
-        //$serviceLocator = $e->getTarget()->getServicemanager();
-        //
-        ///** @var Config $config */
-        //$config = $serviceLocator->get(self::class);
-        //
-        ///** @var TreeRouteStack $router */
-        //$router = $serviceLocator->get('Router');
-        //
-        //if ($config->get('enabled') && $router instanceof TreeRouteStack) {
-        //    $serviceLocator->get('Router')->addRoutes([
-        //        self::class => [
-        //            'type' => Segment::class,
-        //            'options' => [
-        //                'route' => $config->get('url'),
-        //                'defaults' => [
-        //                    'controller' => IndexController::class,
-        //                    'action' => 'dispatch'
-        //                ]
-        //            ],
-        //            'child_routes' => [
-        //                [
-        //                    'type' => Method::class,
-        //                    'options' => [
-        //                        'verb' => 'post'
-        //                    ]
-        //                ]
-        //            ]
-        //        ]
-        //    ]);
-        //}
     }
 
     /**
